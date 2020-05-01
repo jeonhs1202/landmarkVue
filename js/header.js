be5c5d76aa2d4e8da11288a2257d0e49cf00aafa
jeonhs <jeonhs1202@gmail.com>
2020-05-01 19:43:52 +0900

    헤더 변경시 로고변경

--- js/header.js
+++ js/header.js
@@ -1,5 +1,8 @@
 Vue.component('header-item',{
-    props: ['name'],
+    props: {
+      name: String,
+      pageNum: Number,
+    },
     template:
      `
      <div id="_header" class="menuBack">
@@ -8,19 +11,23 @@ Vue.component('header-item',{
      </div>
      <div class="buttonBar">
          <button class="menuButton" onclick="location.href='myTrip.html'">
-             <img src="../img/myTravelList.png" class="menuImg">
+             <img src="../img/myTravelListChecked.png" class="menuImg" v-if="pageNum=='1'">
+             <img src="../img/myTravelList.png" class="menuImg" v-else>
              내 여행 보기
          </button>
          <button class="menuButton" onclick="location.href='landMarkSearch.html'">
-             <img src="../img/landMarkSearch.png" class="menuImg">
+             <img src="../img/landMarkSearchChecked.png" class="menuImg" v-if="pageNum=='2'">
+             <img src="../img/landMarkSearch.png" class="menuImg" v-else>
              관광지 검색
          </button>
          <button class="menuButton" onclick="location.href='myLandMark.html'">
-             <img src="../img/myLandMarkManage.png" class="menuImg">
+             <img src="../img/myLandMarkManageChecked.png" class="menuImg" v-if="pageNum=='3'">
+             <img src="../img/myLandMarkManage.png" class="menuImg" v-else>
              내 관광지 관리
          </button>
          <button class="menuButton" onclick="location.href='qna.html'">
-             <img src="../img/QnA.png" class="menuImg">
+             <img src="../img/QnAChecked.png" class="menuImg" v-if="pageNum=='4'">
+             <img src="../img/QnA.png" class="menuImg" v-else>
              문의 사항
          </button>
      </div>
@@ -40,7 +47,7 @@ Vue.component('header-item',{
   var header = new Vue({
     el: '#_header',
     data: {
-        name: '혜선'
+        name: '혜선',
     }
     })
   
