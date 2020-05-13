8dbfd3850f4520c1af40a8a6b6c2d45b6cb59347
grappe96 <wook0805@hanmail.net>
2020-05-13 17:32:07 +0900

    내 여행 조회 화면 추가

---
+++ js/myTrip.js
@@ -0,0 +1,88 @@
+var trip = {
+    template: `<div class="travel">
+                    <li v-for="trip in triplist">
+                        <img v-if="trip.firstImage2 == null" src="../img/temptrip.jpg" class="tripImg">
+                        <button>{{ trip.title }}</button>
+                    </li>
+                </div>`
+    ,
+    props: {
+        triplist: {
+            id: null,
+            userId: null,
+            addr1: null,
+            addr2: null,
+            areaCode: null,
+            sigunguCode: null,
+            cat1: null,
+            cat2: null,
+            cat3: null,
+            contentId: null,
+            contentTypeId: null,
+            tel: null,
+            title: null,
+            overview: null,
+            createdTime: null,
+            modifiedTime: null,
+            firstImage: null,
+            firstImage2: null,
+            homepage: null,
+            readCount: null
+        }
+    }
+}
+
+var myTrip = new Vue({
+    el: '#_mytrip',
+    data: {
+        content: [
+            {
+                id: 1,
+                userId: 0,
+                addr1: "전라북도 순창군 적성면",
+                addr2: null,
+                areaCode: 37,
+                sigunguCode: 7,
+                cat1: "A02",
+                cat2: "A0205",
+                cat3: "A02050100",
+                contentId: 2654773,
+                contentTypeId: 12,
+                tel: null,
+                title: "순창군 채계산 출렁다리",
+                overview: null,
+                createdTime: "2020-04-28 16:14",
+                modifiedTime: "2020-04-28 16:14",
+                firstImage: null,
+                firstImage2: null,
+                homepage: null,
+                readCount: 0
+            },
+            {
+                id: 2,
+                userId: 0,
+                addr1: "충청남도 계룡시 두마면 입암길 218",
+                addr2: null,
+                areaCode: 34,
+                sigunguCode: 16,
+                cat1: "A01",
+                cat2: "A0101",
+                cat3: "A01011700",
+                contentId: 2654766,
+                contentTypeId: 12,
+                tel: null,
+                title: "계룡 입암저수지",
+                overview: null,
+                createdTime: "2020-04-28 16:05",
+                modifiedTime: "2020-04-28 16:14",
+                firstImage: null,
+                firstImage2: null,
+                homepage: null,
+                readCount: 0
+            }
+        ]
+    },
+    components: {
+        'my-trip': trip
+    }
+})
