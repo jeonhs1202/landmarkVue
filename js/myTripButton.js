8ea8b9cc9fa3c658536e48ef0f3e197a82b058e3
jeonhs <jeonhs1202@gmail.com>
2020-05-17 14:51:35 +0900

    메인페이지 버튼 정렬

--- js/myTripButton.js
+++ js/myTripButton.js
@@ -17,26 +17,35 @@ var myButton = new Vue({
     el: '#_mybutton',
     data: {
           citybutton: [
-            {name: '서울', level: 'one'}, 
-            {name: '인천', level: 'two'},
-            {name: '대전', level: 'three'}, 
-            {name: '대구', level: 'four'}, 
-            {name: '광주', level: 'five'}, 
-            {name: '부산', level: 'six'}, 
-            {name: '울산', level: 'seven'}, 
-            {name: '세종특별자치시', level: 'eight'},
-            {name: '경기도', level: 'nine'}, 
-            {name: '강원도', level: 'ten'}, 
-            {name: '충청북도', level: 'nine'}, 
-            {name: '충청남도', level: 'eight'}, 
-            {name: '경상북도', level: 'seven'}, 
-            {name: '경상남도', level: 'six'}, 
-            {name: '전라북도', level: 'five'}, 
-            {name: '전라남도', level: 'four'}, 
-            {name: '제주도', level: 'three'}
+            {name: '서울', level: 'a'}, 
+            {name: '인천', level: 'b'},
+            {name: '대전', level: 'c'}, 
+            {name: '대구', level: 'd'}, 
+            {name: '광주', level: 'e'}, 
+            {name: '부산', level: 'f'}, 
+            {name: '울산', level: 'g'}, 
+            {name: '세종특별자치시', level: 'h'},
+            {name: '경기도', level: 'i'}, 
+            {name: '강원도', level: 'j'}, 
+            {name: '충청북도', level: 'i'}, 
+            {name: '충청남도', level: 'h'}, 
+            {name: '경상북도', level: 'g'}, 
+            {name: '경상남도', level: 'f'}, 
+            {name: '전라북도', level: 'e'}, 
+            {name: '전라남도', level: 'd'}, 
+            {name: '제주도', level: 'c'}
         ]      
+    },
+    computed: {
+        citybuttonsort : function() {
+            function compare(a, b) {
+              return (a.level > b.level) ? -1 : 1;
+            }        
+            return this.citybutton.sort(compare);
+          }
     },
     components: {
         'my-button': city
-    }
+    },
+    
 })
