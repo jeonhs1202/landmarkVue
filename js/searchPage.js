3bf3f418c3ce4a9187cba8885d25040e57a8faac
jeonhs <jeonhs1202@gmail.com>
2020-05-19 20:02:41 +0900

    관광지 검색바 컴포넌트화

--- js/searchPage.js
+++ js/searchPage.js
@@ -1,3 +1,47 @@
+Vue.component('searchBar',{
+    props:{
+        landMarkType: String,
+        city: String,
+        country: String,
+    },
+    template: 
+    `<div class="searchBar">
+        <div class="searchBarItem">
+            <select v-model="landMarkType">
+                <option disabled value="">관광지 타입</option>
+                <option>바다</option>
+                <option>산</option>
+                <option>숙소</option>
+            </select>
+            <select v-model="city">
+                <option disabled value="">대분류</option>
+                <option>서울특별시</option>
+                <option>경기도</option>
+                <option>강원도</option>
+            </select>
+            <select v-model="country">
+                <option disabled value="">세부 분류</option>
+                <option>강북구</option>
+                <option>노원구</option>
+                <option>강원도</option>
+            </select>
+            
+            <input type="text" placeholder="검색어를 입력하세요">
+            <button v-on:click="passData(landMarkType, city, country)">검색</button>
+        </div>
+    </div>`,
+    methods:{
+        passData: function(type, city, country) {
+            if(event)
+                this.$emit('type', type);
+                this.$emit('city', city);
+                this.$emit('country', country);
+        }
+    }
+})
+
+
+
 var searchPgage = new Vue({
     el: '#_searchPage',
     data: {
@@ -5,5 +49,16 @@ var searchPgage = new Vue({
         city: '',
         country: '',
         landMarkType: '',
+    },
+    methods:{
+        typeset: function(value){
+            this.landMarkType = value;
+        },
+        cityset: function(value){
+            this.city = value;
+        },
+        countryset: function(value){
+            this.country = value;
+        },
     }
     })
