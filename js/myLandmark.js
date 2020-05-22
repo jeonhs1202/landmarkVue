400f24e6eb0c372087ed9ea41d50d23fc171761c
grappe96 <wook0805@hanmail.net>
2020-05-22 16:57:30 +0900

    내 관광지 관리 수정

--- js/myLandmark.js
+++ js/myLandmark.js
@@ -1,23 +1,18 @@
 var admin = {
     template: `<div>
                     <div class="btnArea">
-                        <button class="btn" v-on:click="clickReview" :class="{click : review}">이용객들 후기 관리</button>
-                        <button class="btn" v-on:click="clickLandmark" :class="{click : landmark}">관광지 관리</button>
+                        <button class="btn1" v-on:click="clickReview" :class="[rv ? 'click' : 'non']">이용객들 후기 관리</button>
+                        <button class="btn2" v-on:click="clickLandmark" :class="[lm ? 'click' : 'non']">관광지 관리</button>
                     </div>
-                    <div class="adminContent"></div>
+                    <div class="adminContent">{{ content }}</div>
                 </div>`,
-    props: {
-        review: false,
-        landmark: false
-    },
+    props: ['rv', 'lm', 'content'],
     methods: {
         clickReview: function () {
-            this.review = true;
-            this.landmark = false;
+            this.$emit('click', 1);
         },
         clickLandmark: function () {
-            this.review = false;
-            this.landmark = true;
+            this.$emit('click', 2);
         }
     }
 }
@@ -26,5 +21,25 @@ var myTrip = new Vue({
     el: '#_mylandmark',
     components: {
         'my-landmark': admin
+    },
+    data: {
+        review: false,
+        landmark: false,
+        RVcontent: '이용객 후기 목록',
+        LMcontent: '관광지 목록',
+        pass: ''
+    },
+    methods: {
+        whichBtn: function (value) {
+            if(value == 1){
+                this.review = true;
+                this.landmark = false;
+                this.pass = this.RVcontent;
+            }else if(value == 2){
+                this.review = false;
+                this.landmark = true;
+                this.pass = this.LMcontent;
+            }
+        }
     }
 })
