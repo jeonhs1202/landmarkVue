e77636004fef0080331f32af543017ba4fc8f6b7
grappe96 <wook0805@hanmail.net>
2020-06-14 21:39:58 +0900

    내 여행보기 수정

--- js/myTrip.js
+++ js/myTrip.js
@@ -22,11 +22,6 @@ var menu = {
 var trip = {
     template: `<div class="travel">
                     <li v-for="trip in triplist">
-<<<<<<< Updated upstream
-                        <div v-if="tripid===trip.id">
-                            <img v-if="trip.firstImage2 == null" src="../img/temptrip.jpg" class="tripImg">
-                            <button >{{ trip.title }}</button>
-=======
                         <div v-if="tripid == trip.id">
                             <div class="title">{{ trip.title }}</div>
                             <div class="time" v-if="trip.modified === null">{{ trip.createdTime }}</div>
@@ -34,7 +29,6 @@ var trip = {
                             <hr class="line">
                             <img v-if="trip.firstImage == null" src="../img/temptrip.jpg" class="tripImg">
                             <div class="content">{{ trip.overview }}</div>
->>>>>>> Stashed changes
                         </div>
                     </li>
                 </div>`
@@ -45,52 +39,7 @@ var trip = {
 var myTrip = new Vue({
     el: '#_mytrip',
     data: {
-        content: [
-            {
-                id: 1,
-                userId: 0,
-                addr1: "전라북도 순창군 적성면",
-                addr2: null,
-                areaCode: 37,
-                sigunguCode: 7,
-                cat1: "A02",
-                cat2: "A0205",
-                cat3: "A02050100",
-                contentId: 2654773,
-                contentTypeId: 12,
-                tel: null,
-                title: "순창군 채계산 출렁다리",
-                overview: null,
-                createdTime: "2020-04-28 16:14",
-                modifiedTime: "2020-04-28 16:14",
-                firstImage: null,
-                firstImage2: null,
-                homepage: null,
-                readCount: 0
-            },
-            {
-                id: 2,
-                userId: 0,
-                addr1: "충청남도 계룡시 두마면 입암길 218",
-                addr2: null,
-                areaCode: 34,
-                sigunguCode: 16,
-                cat1: "A01",
-                cat2: "A0101",
-                cat3: "A01011700",
-                contentId: 2654766,
-                contentTypeId: 12,
-                tel: null,
-                title: "계룡 입암저수지",
-                overview: null,
-                createdTime: "2020-04-28 16:05",
-                modifiedTime: "2020-04-28 16:14",
-                firstImage: null,
-                firstImage2: null,
-                homepage: null,
-                readCount: 0
-            }
-        ],
+        content: [],
         itemId: 0
     },
     created: function(){
@@ -99,8 +48,6 @@ var myTrip = new Vue({
             let vars = uri[1].split('=');
             this.itemId = parseInt(vars[1]);
         }
-<<<<<<< Updated upstream
-=======
         axios.post('http://49.50.161.45:8080/review/search', {
             type: 0
         }, {
@@ -111,7 +58,6 @@ var myTrip = new Vue({
             //console.log(res);
             this.content = (res.data);
         });
->>>>>>> Stashed changes
     },
     components: {
         'side-bar': menu,
