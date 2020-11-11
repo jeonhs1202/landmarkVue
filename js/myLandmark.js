6ff37ff2bc15bad836b7c7a83428d8aa06964eec
grappe96 <wook0805@hanmail.net>
2020-11-12 02:33:31 +0900

    이용객 후기 관리 게시판 틀

--- js/myLandmark.js
+++ js/myLandmark.js
@@ -1,45 +1,91 @@
 var admin = {
-    template: `<div>
+  template: `<div>
                     <div class="btnArea">
                         <button class="btn1" v-on:click="clickReview" :class="[rv ? 'click' : 'non']">이용객들 후기 관리</button>
                         <button class="btn2" v-on:click="clickLandmark" :class="[lm ? 'click' : 'non']">관광지 관리</button>
                     </div>
-                    <div class="adminContent">{{ content }}</div>
+                    <div class="adminContent">
+                        <template v-if="detail">
+                            <div v-for="content in contents" v-if="id === content.id"></div>
+                        </template>
+                        <template v-else>
+                            <table class="tb">
+                                <div v-for="content in contents">
+                                    <tr>
+                                        <td>
+                                            {{content.id}}
+                                        </td>
+                                        <td>
+                                            {{content.title}}
+                                        </td>
+                                    </tr>
+                                </div>
+                            </table>
+                        </template>
+                    </div>
                 </div>`,
-    props: ['rv', 'lm', 'content'],
-    methods: {
-        clickReview: function () {
-            this.$emit('click', 1);
-        },
-        clickLandmark: function () {
-            this.$emit('click', 2);
-        }
-    }
-}
+  props: ['rv', 'lm', 'contents', 'detail', 'id'],
+  methods: {
+    clickReview: function () {
+      this.$emit('click', 1);
+    },
+    clickLandmark: function () {
+      this.$emit('click', 2);
+    },
+    showDetail: function (id) {
+      this.$emit('detail', true);
+      this.$emit('id', id);
+    },
+  },
+};
 
 var myTrip = new Vue({
-    el: '#_mylandmark',
-    components: {
-        'my-landmark': admin
-    },
-    data: {
-        review: false,
-        landmark: false,
-        RVcontent: '이용객 후기 목록',
-        LMcontent: '관광지 목록',
-        pass: ''
-    },
-    methods: {
-        whichBtn: function (value) {
-            if(value == 1){
-                this.review = true;
-                this.landmark = false;
-                this.pass = this.RVcontent;
-            }else if(value == 2){
-                this.review = false;
-                this.landmark = true;
-                this.pass = this.LMcontent;
-            }
+  el: '#_mylandmark',
+  components: {
+    'my-landmark': admin,
+  },
+  data: {
+    review: true,
+    landmark: false,
+    RVcontentlist: {},
+    LMcontentlist: {},
+    pass: {},
+    detail: false,
+    id: 0,
+  },
+  created: function () {
+    const baseURI = 'http://49.50.161.45:8080';
+    axios
+      .post(
+        `${baseURI}/review/search`,
+        {
+          type: 0,
+        },
+        {
+          headers: {
+            'auth-token': window.localStorage.getItem('token'),
+          },
         }
-    }
-})
+      )
+      .then((res) => {
+        this.RVcontentlist = res.data;
+        this.pass = this.RVcontentlist;
+      })
+      .catch((ex) => {
+        console.log(ex);
+      });
+  },
+  methods: {
+    whichBtn: function (value) {
+      if (value == 1) {
+        this.review = true;
+        this.landmark = false;
+        this.pass = this.RVcontentlist;
+      } else if (value == 2) {
+        this.review = false;
+        this.landmark = true;
+        this.pass = this.LMcontentlist;
+      }
+    },
+  },
+});
