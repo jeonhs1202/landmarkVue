185b7ff62bed10142698f8795e6ff249b3368fcf
grappe96 <wook0805@hanmail.net>
2020-11-12 23:12:02 +0900

    이용객 후기 관리 게시판 css, 상세화면 틀

--- js/myLandmark.js
+++ js/myLandmark.js
@@ -1,30 +1,50 @@
 var admin = {
-  template: `<div>
-                    <div class="btnArea">
-                        <button class="btn1" v-on:click="clickReview" :class="[rv ? 'click' : 'non']">이용객들 후기 관리</button>
-                        <button class="btn2" v-on:click="clickLandmark" :class="[lm ? 'click' : 'non']">관광지 관리</button>
-                    </div>
-                    <div class="adminContent">
-                        <template v-if="detail">
-                            <div v-for="content in contents" v-if="id === content.id"></div>
-                        </template>
-                        <template v-else>
-                            <table class="tb">
-                                <div v-for="content in contents">
-                                    <tr>
-                                        <td>
-                                            {{content.id}}
-                                        </td>
-                                        <td>
-                                            {{content.title}}
-                                        </td>
-                                    </tr>
-                                </div>
-                            </table>
-                        </template>
-                    </div>
-                </div>`,
-  props: ['rv', 'lm', 'contents', 'detail', 'id'],
+  template: `
+    <div>
+      <div class="btnArea">
+          <button class="btn1" v-on:click="clickReview" :class="[rv ? 'click' : 'non']">이용객들 후기 관리</button>
+          <button class="btn2" v-on:click="clickLandmark" :class="[lm ? 'click' : 'non']">관광지 관리</button>
+      </div>
+      <div class="adminContent">
+          <div>
+            <div class="tb">
+              <table>
+                <colgroup>
+                  <col width="40%" />
+                  <col width="60%" />
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th> 후기 번호 </th>
+                    <th> 제목 </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="content in contents">
+                    <td>
+                        {{content.id}}
+                    </td>
+                    <td>
+                        <button class="detailBtn" @click="showDetail(content.id, content.areaCode, content.sigunguCode)">{{content.title}}</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="detail">
+                <div v-for="content in contents" v-if="id === content.id">
+                  {{ city }}
+                  {{ sigungu }}
+                  작성 시각: {{ content.createdTime }}
+                  수정 시각: {{ content.modifiedTime }}
+                  제목 : {{ content.title }}
+                  내용 : {{ content.overview }}
+                </div>
+            </div>
+          </div>
+      </div>
+    </div>`,
+  props: ['rv', 'lm', 'contents', 'id', 'city', 'sigungu'],
   methods: {
     clickReview: function () {
       this.$emit('click', 1);
@@ -32,9 +52,10 @@ var admin = {
     clickLandmark: function () {
       this.$emit('click', 2);
     },
-    showDetail: function (id) {
-      this.$emit('detail', true);
+    showDetail: function (id, areaCode, sigunguCode) {
       this.$emit('id', id);
+      this.$emit('cname', areaCode);
+      this.$emit('sname', sigunguCode);
     },
   },
 };
@@ -49,18 +70,26 @@ var myTrip = new Vue({
     landmark: false,
     RVcontentlist: {},
     LMcontentlist: {},
+    citylist: [],
+    sigungulist: [],
+    city: '',
+    sigungu: '',
+    areacode: 0,
     pass: {},
-    detail: false,
     id: 0,
   },
   created: function () {
     const baseURI = 'http://49.50.161.45:8080';
+    axios.get(`${baseURI}/code/area`).then((res) => {
+      this.citylist = res.data;
+    });
+    axios.get(`${baseURI}/code/sigungu`).then((res) => {
+      this.sigungulist = res.data;
+    });
     axios
       .post(
         `${baseURI}/review/search`,
-        {
-          type: 0,
-        },
+        { type: 0 },
         {
           headers: {
             'auth-token': window.localStorage.getItem('token'),
@@ -85,6 +114,31 @@ var myTrip = new Vue({
         this.review = false;
         this.landmark = true;
         this.pass = this.LMcontentlist;
+      }
+    },
+    idset: function (value) {
+      this.id = value;
+    },
+    cname: function (value) {
+      let len = this.citylist.length;
+      for (let i = 0; i < len; i++) {
+        if (this.citylist[i].code == value) {
+          this.city = this.citylist[i].name;
+          this.areacode = value;
+          break;
+        }
+      }
+    },
+    sname: function (value) {
+      let len = this.sigungulist.length;
+      for (let i = 0; i < len; i++) {
+        if (
+          this.sigungulist[i].areaCode == this.areacode &&
+          this.sigungulist[i].code == value
+        ) {
+          this.sigungu = this.sigungulist[i].name;
+          break;
+        }
       }
     },
   },
